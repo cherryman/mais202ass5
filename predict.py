87d2f220b34fca5a3e5f5398da6b292bee1c4da8
Sheheryar Parvaz <me@cherryman.org>
2020-11-18 23:37:15 -0500

    Fix model weights in predict.py

--- predict.py
+++ predict.py
@@ -3,17 +3,19 @@
 import numpy as np
 import torch
 from net import Net
-from utils import norm, show
+from utils import device
 
-weights_path = "model-state-dict-epoch-1-31.06-val-loss.pt"
+weights_path = "model-state-dict-epoch-43-209.18-val-loss.pt"
 net = Net()
 net.load_state_dict(torch.load(weights_path))
+net.to(device)
+net.eval()
 
 X_test = np.load("test_x.npy")
 for im in X_test:
-    im[im<220] = 0
+    im[im < 220] = 0
 
-X_test = torch.from_numpy(X_test).unsqueeze(1).unsqueeze(1)
+X_test = torch.from_numpy(X_test).unsqueeze(1).unsqueeze(1).to(device)
 y_preds = []
 
 with torch.no_grad():
